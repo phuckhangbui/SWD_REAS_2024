3694e89e444c134d4f8d6a9c94db82c0d02ee403
Nguyen Van Hoang Minh <Ngminh3004@gmail.com>
2024-01-30 22:36:27 +0700

    update database

--- API/Entity/Account.cs
+++ API/Entity/Account.cs
@@ -4,13 +4,14 @@ public class Account
 {
     public int AccountId { get; set; }
     public string AccountName { get; set; }
+    public byte[] PasswordHash { get; set; }
+    public byte[] PasswordSalt { get; set; }
     public string PhoneNumber { get; set; }
     public string Citizen_identification { get; set; }
     public string Address { get; set; }
     public Major Major { get; set; }
     public int MajorId { get; set; }
     public Role Role { get; set; }
-
     public int RoleId { get; set; }
     public int Account_Status { get; set; }
     public DateTime Date_Created { get; set; } = DateTime.UtcNow;
