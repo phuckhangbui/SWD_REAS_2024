e54a47cdf5fedced69705a97e4142587acce3ef7
phuckhangbui <khangnguyenphucbui@gmail.com>
2024-01-31 08:10:19 +0700

    Create base controller, setup Errors, Add Dtos, Add Token Services

---
+++ API/Entity/Identity/AppUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace API.Entity.Identity
+{
+    public class AppUser : IdentityUser
+    {
+        public string DisplayName { get; set; }
+        public string AccountEmail { get; set; }
+    }
+}
