8a22ec7be41cae7ced7c3f5d3656ea3c053c6ae3
ChonkySeals <trivm182003@gmail.com>
2024-02-02 11:01:09 +0700

    Add google sign in

--- web-app/src/index.js
+++ web-app/src/index.js
@@ -2,15 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { GoogleOAuthProvider } from '@react-oauth/google';
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { AuthProvider } from "./context/AuthProvider";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID
+const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId={clientId}>
-      <App />
-    </GoogleOAuthProvider>
+    <BrowserRouter>
+      <AuthProvider>
+        <GoogleOAuthProvider clientId={clientId}>
+          <App />
+        </GoogleOAuthProvider>
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
-
