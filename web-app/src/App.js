8d0717ac6548820d2f042eea8a203c9937ba9132
ChonkySeals <trivm182003@gmail.com>
2024-02-02 08:50:29 +0700

    Create react app

---
+++ web-app/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import GoogleLogIn from "./components/GoogleLogIn/GoogleLogIn";
+
+function App() {
+  return (
+    <div className="App">
+      <GoogleLogIn />
+    </div>
+  );
+}
+
+export default App;
