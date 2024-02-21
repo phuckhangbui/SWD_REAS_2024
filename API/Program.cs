a7487bd2078343ef70a09a35a949aa5fe5722a63
Minh Nguyên <108600635+Nguyennm080503@users.noreply.github.com>
2024-02-21 08:43:38 +0700

    update

--- API/Program.cs
+++ API/Program.cs
@@ -9,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.ApplicationServices(builder.Configuration);
+builder.Services.IdentityServices(builder.Configuration);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
