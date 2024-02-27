0ac7f433fbee1fb7ff665cea9e952a006396f5c7
Minh Nguyên <108600635+Nguyennm080503@users.noreply.github.com>
2024-02-27 22:07:32 +0700

    update

--- API/Entity/News.cs
+++ API/Entity/News.cs
@@ -5,6 +5,7 @@ public class News
     public int NewsId { get; set; }
     public Account AccountCreate { get; set; }
     public int AccountCreateId { get; set; }
+    public string Thumbnail { get; set; }
     public string AccountName { get; set; }
     public string NewsTitle { get; set; }
     public string NewsSumary { get; set; }
